4db2078bfc5324195619fb5f528017aef38191ff
Zone_Infinity <68643247+Zone-Infinity@users.noreply.github.com>
2022-01-10 15:10:13 +0530

    Don't mention it

--- src/main/java/bot/Secrets.java
+++ src/main/java/bot/Secrets.java
@@ -1,6 +1,6 @@
 package bot;
 
 public class Secrets {
-    public static String TOKEN = "NzkwNzcwOTEzNzE2NDY5Nzcx.X-FczQ.XPH6MQP8YstpfWpqaQ_YarvliRU";
-    public static String DatabaseURI = "mongodb+srv://BooleanCube:golemcube@golemcube.91qk1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    public static String TOKEN = "TOKEN";
+    public static String DatabaseURI = "mongodb+srv://BooleanCube:sussy@cube.91qk1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 }
