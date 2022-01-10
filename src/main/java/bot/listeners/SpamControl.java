23439c305de617367e96b30726248cebcb739127
Zone_Infinity <sohamdokhale1602@gmail.com>
2022-01-10 15:08:35 +0530

    Refactor SpamControl

--- src/main/java/bot/listeners/SpamControl.java
+++ src/main/java/bot/listeners/SpamControl.java
@@ -5,8 +5,6 @@ import bot.Settings;
 import bot.Tools;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.Role;
-import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -14,12 +12,12 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Objects;
 
+@SuppressWarnings("ConstantConditions")
 public class SpamControl extends ListenerAdapter {
-    static ArrayList<TextChannel> blackListedChannels = new ArrayList<>();
-    static ArrayList<Member> blackListedMembers = new ArrayList<>();
-    static ArrayList<Role> blackListedRoles = new ArrayList<>();
+    static ArrayList<String> blackListedChannels = new ArrayList<>();
+    static ArrayList<String> blackListedMembers = new ArrayList<>();
+    static ArrayList<String> blackListedRoles = new ArrayList<>();
 
     HashMap<Member, MessageHistory> messageTracking = new HashMap<>();
 
@@ -27,47 +25,48 @@ public class SpamControl extends ListenerAdapter {
     public void onMessageReceived(@Nonnull MessageReceivedEvent event) {
         if (!Settings.isEnabled(SettingType.SpamControl)) return;
 
-        //5 messages in under 3 seconds then mute for 5 minutes and add a warning!
-        if (blackListedChannels.contains(event.getChannel()) || blackListedMembers.contains(event.getMember())) {
-            for (Role r : blackListedRoles) {
-                if (Objects.requireNonNull(event.getMember()).getRoles().contains(r)) {
-                    return;
-                }
-            }
+        Member member = event.getMember();
+
+        if (blackListedChannels.contains(event.getChannel().getId())
+                || blackListedMembers.contains(member.getId())
+                || member.getRoles().stream().anyMatch(it -> blackListedRoles.contains(it.getId()))) {
             return;
         }
+
+        // 9 messages in under 3 seconds then mute for 5 minutes and add a warning!
         int scmessages = 9;
         int scseconds = 3;
-        if (!messageTracking.containsKey(event.getMember())) {
-            messageTracking.put(event.getMember(), new MessageHistory(1, System.currentTimeMillis()));
+
+        if (!messageTracking.containsKey(member)) {
+            messageTracking.put(member, new MessageHistory(1, System.currentTimeMillis()));
         } else {
-            int msgNum = messageTracking.get(event.getMember()).msgNum++;
-            long lastTimeSent = messageTracking.get(event.getMember()).lastTimeSent;
+            int msgNum = messageTracking.get(member).msgNum++;
+            long lastTimeSent = messageTracking.get(member).lastTimeSent;
             if (msgNum == scmessages && System.currentTimeMillis() - lastTimeSent <= scseconds * 1000) {
-                Tools.muteMember(event.getMember(), event.getGuild(), 5, "Spamming");
-                Objects.requireNonNull(event.getMember()).getUser().openPrivateChannel().queue(c -> {
+                Tools.muteMember(member, event.getGuild(), 5, "Spamming");
+                member.getUser().openPrivateChannel().queue(c -> {
                     c.sendMessageEmbeds(new EmbedBuilder().setDescription("You have been muted for **5 minutes** for spamming in a channel! You have also been given **1 warning**!").build()).queue();
                 });
                 event.getChannel().sendMessage("Please do not spam! You have been muted for `5 minutes`!").queue();
-                if (Tools.memberToWarns.get(event.getMember()).size() >= 4) {
-                    event.getChannel().sendMessage("Kicked " + event.getMember().getAsMention() + " from the server because they exceeded `3 warnings`!").queue();
-                    event.getMember().kick("Exceeded 3 warnings!").queue();
+                if (Tools.memberToWarns.get(member).size() >= 4) {
+                    event.getChannel().sendMessage("Kicked " + member.getAsMention() + " from the server because they exceeded `3 warnings`!").queue();
+                    member.kick("Exceeded 3 warnings!").queue();
                 }
             } else if (System.currentTimeMillis() - lastTimeSent > scseconds * 1000) {
-                messageTracking.get(event.getMember()).msgNum = 1;
-                messageTracking.get(event.getMember()).lastTimeSent = System.currentTimeMillis();
+                messageTracking.get(member).msgNum = 1;
+                messageTracking.get(member).lastTimeSent = System.currentTimeMillis();
             }
-            messageTracking.get(event.getMember()).lastTimeSent = System.currentTimeMillis();
+            messageTracking.get(member).lastTimeSent = System.currentTimeMillis();
         }
     }
 
     @Override
     public void onReady(@Nonnull ReadyEvent event) {
         //Add all of your blacklists here
-        blackListedChannels.add(event.getJDA().getGuilds().get(0).getTextChannelById(768793442632990721L));
-        blackListedChannels.add(event.getJDA().getGuilds().get(0).getTextChannelById(741785877944074251L));
-        blackListedMembers.add(event.getJDA().getGuilds().get(0).getOwner());
-        blackListedRoles.add(event.getJDA().getGuilds().get(0).getRoleById(773337238952083477L));
+        blackListedChannels.add("768793442632990721");
+        blackListedChannels.add("741785877944074251");
+        blackListedMembers.add(event.getJDA().getGuilds().get(0).getOwner().getId());
+        blackListedRoles.add("773337238952083477");
     }
 }
 
