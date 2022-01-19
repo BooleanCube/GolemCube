c0caa194f823e8b902faed0440b80b115604e9c5
V-Play-Games <vaibhavnargwani28@gmail>
2022-01-19 17:44:24 +0530

    Minor improvements in Database.java Cached the filter in Database for better speed Use executor instead of making new threads everytime

--- src/main/java/bot/database/Database.java
+++ src/main/java/bot/database/Database.java
@@ -6,11 +6,11 @@ import com.mongodb.client.model.Updates;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.ReadyEvent;
-import net.dv8tion.jda.api.events.ShutdownEvent;
 import net.dv8tion.jda.api.events.user.update.UserUpdateDiscriminatorEvent;
 import net.dv8tion.jda.api.events.user.update.UserUpdateNameEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,20 +19,20 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Database extends ListenerAdapter {
-    static Logger LOGGER = LoggerFactory.getLogger(Database.class);
-
-    static MongoClient client;
-    static MongoDatabase db;
-    static MongoCollection<Document> reputationCollection;
-
-    static HashMap<String, Integer> board = new HashMap<>();
-    static HashMap<String, String> nameCache = new HashMap<>();
-
-    static String reputationClusterName = "ReputationTracker";
+    private static final Logger LOGGER = LoggerFactory.getLogger(Database.class);
+    private static final ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(2);
+    private static final HashMap<String, Integer> board = new HashMap<>();
+    private static final HashMap<String, String> nameCache = new HashMap<>();
+    private static final String reputationClusterName = "ReputationTracker";
+    private static final Bson filter = Filters.eq("_id", reputationClusterName);
+    private static MongoClient client;
+    private static MongoDatabase db;
+    private static MongoCollection<Document> reputationCollection;
 
     public static void setupDatabase(String DatabaseURI) {
         client = MongoClients.create(DatabaseURI);
@@ -45,8 +45,8 @@ public class Database extends ListenerAdapter {
 
         reputationCollection = db.getCollection(reputationClusterName);
 
-        if (reputationCollection.countDocuments(Filters.eq("_id", reputationClusterName)) == 0) {
-            reputationCollection.insertOne(new Document().append("_id", reputationClusterName));
+        if (reputationCollection.countDocuments(filter) == 0) {
+            reputationCollection.insertOne(new Document("_id", reputationClusterName));
             LOGGER.info("Created {} in {} collection", reputationClusterName, reputationClusterName);
         }
 
@@ -55,14 +55,9 @@ public class Database extends ListenerAdapter {
 
     public static void addReputation(Member m) {
         String id = m.getId();
-        int reps = board.computeIfAbsent(id, a -> 0) + 1;
-
-        new Thread(() ->
-                reputationCollection.findOneAndUpdate(Filters.eq("_id", reputationClusterName),
-                        Updates.set(id, reps))
-        ).start();
+        Integer reps = getReputation(m) + 1;
+        executor.execute(() -> reputationCollection.findOneAndUpdate(filter, Updates.set(id, 1)));
         board.put(id, reps);
-
         LOGGER.info("Updated reputation of {} to {}", m.getUser().getAsTag(), reps);
     }
 
@@ -121,12 +116,11 @@ public class Database extends ListenerAdapter {
     public void onReady(@NotNull ReadyEvent event) {
         //cache the database in a hashmap which is then updated frequently in a separate thread
         //this was done for optimization, because retrieving data from a cloud based database is going to take forever
-        try (MongoCursor<Document> cursor =
-                     reputationCollection.find(Filters.eq("_id", reputationClusterName)).iterator()) {
+        try (MongoCursor<Document> cursor = reputationCollection.find(filter).iterator()) {
             Document reputationTracker = cursor.next();
 
             for (String id : reputationTracker.keySet()) {
-                if(id.equals("_id")) continue;
+                if (id.equals("_id")) continue;
                 board.put(id, reputationTracker.getInteger(id));
             }
         }
@@ -150,5 +144,4 @@ public class Database extends ListenerAdapter {
         if (user.isBot()) return;
         nameCache.put(user.getId(), user.getAsTag());
     }
-
 }
